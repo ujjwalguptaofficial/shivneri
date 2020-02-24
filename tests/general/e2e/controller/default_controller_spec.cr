a8d1d6fa62240d993f843dc316173425a26608f2
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2020-02-24 21:20:05 +0530

    check for 405

--- tests/general/e2e/controller/default_controller_spec.cr
+++ tests/general/e2e/controller/default_controller_spec.cr
@@ -16,10 +16,41 @@ describe "DefaultController" do
     response.body.tr("\n", "").tr(" ", "").should eq "<html><head><title>Welcometofort</title></head><body><h1>Index</h1></body></html>"
   end
 
+  # it "/ with content type text/plain" do
+  #   response = http_client.get("/", HTTP::Headers{
+  #     "Accept" => "text/plain",
+  #   })
+  #   response.status_code.should eq 200
+  #   response.content_type.should eq "text/plain"
+  #   response.headers["x-powered-by"]?.should eq "fort"
+  #   response.body.tr("\n", "").tr(" ", "").should eq "<html><head><title>Welcometofort</title></head><body><h1>Index</h1></body></html>"
+  # end
+
   it "index1 - multiple assign" do
     response = http_client.get("/index1")
     response.status_code.should eq 200
     response.body.should eq "UjjwalGupta"
+  end
+
+  it "get friends" do
+    response = http_client.get("/friends")
+    response.status_code.should eq 200
+    response.content_type.should eq "application/json"
+    response.body.should eq "{\"friends\":[\"mohan\",\"sohan\"]}"
+  end
+
+  it "post friends" do
+    response = http_client.post("/friends")
+    response.status_code.should eq 200
+    response.content_type.should eq "application/json"
+    response.body.should eq "{\"friends\":[\"mohan\",\"sohan\"]}"
+  end
+
+  it "put friends" do
+    response = http_client.put("/friends")
+    response.status_code.should eq 405
+    response.content_type.should eq "text/html"
+    response.body.should eq "<h1>Method Not allowed.</h1>"
   end
 
   Spec.after_suite do
--- tests/general/e2e/spec_helper.cr
+++ tests/general/e2e/spec_helper.cr
@@ -19,4 +19,28 @@ class HttpClient
 
     return HTTP::Client.get url
   end
+
+  def get(url : String, headers)
+    if (url.includes?("http") == false)
+      url = @base_url + url
+    end
+
+    return HTTP::Client.get(url, headers)
+  end
+
+  def post(url : String)
+    if (url.includes?("http") == false)
+      url = @base_url + url
+    end
+
+    return HTTP::Client.post url
+  end
+
+  def put(url : String)
+    if (url.includes?("http") == false)
+      url = @base_url + url
+    end
+
+    return HTTP::Client.put url
+  end
 end

a456f7aeba587e4904934835f0ce13ff2ec634f6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2020-03-28 00:26:38 +0530

    add emit method in clients

--- src/model/web_socket_clients.cr
+++ src/model/web_socket_clients.cr
@@ -8,9 +8,21 @@ module Shivneri
         return @current_proc.call
       end
 
-      def broadcast
+      def emit(event_name : String, data : String)
+        current.emit(event_name, data)
       end
-      
+
+      def emit(event_name : String, data)
+        current.emit(event_name, data)
+      end
+
+      def emit(event_name : String, data : ALIAS::NumberType)
+        current.emit(event_name, data)
+      end
+
+      def emit(event_name : String, data : Bool)
+        current.emit(event_name, data)
+      end
     end
   end
 end

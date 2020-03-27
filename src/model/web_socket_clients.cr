e9acb9a106a465c2a3d6f5a3a29cacd6fa84fdc6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2020-03-28 03:43:09 +0530

    implment groups

--- src/model/web_socket_clients.cr
+++ src/model/web_socket_clients.cr
@@ -2,33 +2,52 @@ module Shivneri
   module MODEL
     alias WebSocketMap = Hash(String, WebSocketClient)
 
+    struct WebSocketGroups
+      @@groups_as_string = {} of String => Array(String)
+
+      def initialize(@current_proc : Proc(String))
+      end
+
+      def create(group_name : String)
+        @@groups_as_string[group_name] = [] of String
+      end
+
+      def add(group_name : String)
+        add(group_name, @current_proc.call)
+      end
+
+      def add(group_name : String, socket_id : String)
+        if (@@groups_as_string.has_key?(group_name))
+          @@groups_as_string[group_name].push(socket_id)
+        else
+          @@groups_as_string[group_name] = [socket_id]
+        end
+      end
+
+      def emit(group_name : String, message)
+      end
+    end
+
     struct WebSocketClients
       @@socket_store = {} of String => WebSocketMap
-      @@groups_as_string = {} of String => Array(String)
       @socket_id : String
       @controller_name : String = ""
 
       # def initialize(@current_proc : Proc(WebSocketClient))
       # end
 
+      def filters(socket_ids : Array(String))
+        
+      end
+
       def initialize
         @socket_id = UUID.random.to_s
+        @groups = WebSocketGroups.new ->{ @socket_id }
       end
 
       def current
         return @@socket_store[@controller_name][@socket_id]
       end
-
-      def create_group(group_name : String)
-        @@groups_as_string[group_name] = [] of String
-      end
-
-      # def add_to(group_name : String, socket_id : String)
-
-      # end
-
-      # def emit_to(group_name : String)
-      # end
 
       def emit(event_name : String, data)
         @@socket_store[controller_name].each_value do |socket|
